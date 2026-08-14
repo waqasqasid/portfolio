7d08f595e3b225305a1264065b1f6c6e2d13c686
waqasqasid <waqasqasid@gmail.com>
2026-08-14 06:14:44 +0500

    added apache

--- src/constants/data.js
+++ src/constants/data.js
@@ -31,7 +31,7 @@ export const SKILL_GROUPS = [
   {
     category: 'Linux',
     icon: 'Terminal',
-    skills: ['User Management', 'Group Management','File Management','Server Monitering & Management','Disk Management', 'FTP',  'Samba Servers'],
+    skills: ['User Management', 'Group Management','File Management','Server Monitering & Management','Disk Management', 'FTP',  'Samba Servers' , 'Apache'],
   },
   {
     category: 'Docker',
